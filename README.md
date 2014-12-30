054f4c1326e5db1c361b56b557b7a07af063f7a4
Batbold Dashzeveg <vimcode@gmail.com>
2014-12-30 15:29:58 -0500

    added duration, volumue and pipe options

--- README.md
+++ README.md
@@ -5,14 +5,20 @@ Simple Go program useful for alerting end of a long running command execution.
 ```
 Compiling:
 $ go build beep.go
+$ strip beep
 $ cp beep /usr/bin/
 
-Usage:
+Usage: beep [options]
   -c=1: count
-  -f=0.055: frequency
+  -d="default": audio device (hw:0,0)
+  -f=0.088: frequency
   -h: help
+  -l: beep per line via pipe
+  -t=1: time duration (1-100)
+  -v=100: volume (1-100)
 
 Examples:
 $ cp -vr directory target; beep
 $ ffmpeg -i video.mp4 -vn -acodec libmp3lame sound.mp3; beep -c 10 -f 0.046
+$ find ~ -name '*.txt' | beep -l
 ```
--- beep.go
+++ beep.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"math"
@@ -17,55 +18,120 @@ import (
 import "C"
 
 var (
-	flagHelp  = flag.Bool("h", false, "help")
-	flagCount = flag.Int("c", 1, "count")
-	flagFreq  = flag.Float64("f", 0.055, "frequency")
+	flagHelp     = flag.Bool("h", false, "help")
+	flagCount    = flag.Int("c", 1, "count")
+	flagFreq     = flag.Float64("f", 0.088, "frequency")
+	flagVolume   = flag.Int("v", 100, "volume (1-100)")
+	flagDuration = flag.Int("t", 1, "time duration (1-100)")
+	flagDevice   = flag.String("d", "default", "audio device (hw:0,0)")
+	flagLine     = flag.Bool("l", false, "beep per line via pipe")
 )
 
 func main() {
 	var handle *C.snd_pcm_t
-	var device = C.CString("default")
 
 	flag.Parse()
-	var freq = *flagFreq
-	var count = *flagCount
+	help := *flagHelp
+	freq := *flagFreq
+	count := *flagCount
+	volume := *flagVolume
+	duration := *flagDuration
+	device := *flagDevice
+	lineBeep := *flagLine
 
-	if *flagHelp {
+	if help {
+		fmt.Println("beep [options]")
 		flag.PrintDefaults()
 		return
 	}
+	if volume < 0 || volume > 100 {
+		volume = 100
+	}
+	if duration < 1 || duration > 100 {
+		duration = 1
+	}
 
-	code := C.snd_pcm_open(&handle, device, C.SND_PCM_STREAM_PLAYBACK, 0)
+	code := C.snd_pcm_open(&handle, C.CString(device), C.SND_PCM_STREAM_PLAYBACK, 0)
 	if code < 0 {
-		fmt.Println("snd_pcm_open:", C.GoString(C.snd_strerror(code)))
+		fmt.Println("snd_pcm_open:", strerror(code))
 		os.Exit(1)
 	}
 	defer C.snd_pcm_close(handle)
+	defer time.Sleep(time.Millisecond * 100)
 
 	code = C.snd_pcm_set_params(
 		handle,
 		C.SND_PCM_FORMAT_U8,
 		C.SND_PCM_ACCESS_RW_INTERLEAVED,
 		1,
-		48000,
+		44100,
 		1,
-		0)
+		500000)
 	if code < 0 {
-		fmt.Println("snd_pcm_set_params:", C.GoString(C.snd_strerror(code)))
+		fmt.Println("snd_pcm_set_params:", strerror(code))
 		os.Exit(1)
 	}
 
-	buf := make([]byte, 1024*10)
-	space := make([]byte, 1024*5)
+	if lineBeep {
+		beepPerLine(handle, volume, freq)
+		return
+	}
+
+	buf := make([]byte, 1024*10*duration)
+	bar := 127.0 * (float64(volume) / 100.0)
 	for i, _ := range buf[:] {
-		buf[i] = byte(255 * math.Sin(float64(i)*freq))
+		buf[i] = byte(127 + (bar * math.Sin(float64(i)*freq)))
 	}
+	bufLow := make([]byte, 1024*5)
+	for i, _ := range bufLow[:] {
+		bufLow[i] = 127
+	}
+	playback(handle, bufLow)
 	for i := 0; i < count; i++ {
-		n := C.snd_pcm_writei(handle, unsafe.Pointer(&buf[0]), C.snd_pcm_uframes_t(len(buf)))
-		if n < 0 {
-			fmt.Printf("snd_pcm_writei: wrote %d/%d\n", n, len(buf))
-		}
-		C.snd_pcm_writei(handle, unsafe.Pointer(&space[0]), C.snd_pcm_uframes_t(len(space)))
+		playback(handle, buf)
+		playback(handle, bufLow)
 	}
-	time.Sleep(time.Millisecond * 200)
+}
+
+func beepPerLine(handle *C.snd_pcm_t, volume int, freq float64) {
+	buf := make([]byte, 1024*7)
+	bar := 127.0 * (float64(volume) / 100.0)
+	var last byte
+	for i, _ := range buf[:] {
+		if i < 1024*4 {
+			buf[i] = byte(127 + (bar * math.Sin(float64(i)*freq)))
+			last = buf[i]
+		} else {
+			buf[i] = last
+			if last > 127 {
+				last--
+			}
+		}
+	}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		fmt.Print(string(line))
+		if !isPrefix {
+			fmt.Println()
+			playback(handle, buf)
+		}
+	}
+}
+
+func playback(handle *C.snd_pcm_t, buf []byte) {
+	n := C.snd_pcm_writei(handle, unsafe.Pointer(&buf[0]), C.snd_pcm_uframes_t(len(buf)))
+	if n < 0 {
+		code := C.snd_pcm_recover(handle, C.int(n), 0)
+		if code < 0 {
+			fmt.Println("snd_pcm_recover:", strerror(code))
+		}
+	}
+}
+
+func strerror(code C.int) string {
+	return C.GoString(C.snd_strerror(code))
 }
