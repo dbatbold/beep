beb1774549a84c0e3b2244bd99945f7ce928d42f
Batbold Dashzeveg <vimcode@gmail.com>
2015-01-12 15:15:10 -0500

    piano try 2

--- piano.go
+++ piano.go
@@ -43,23 +43,20 @@ func (p *Piano) GenerateNote(freq float64, duration int) []byte {
 	timer1 := 0.0
 	timer2 := 0.0
 	timer3 := 0.0
-	timer4 := 0.0
 	tick := 2*math.Pi/sampleRate64*freq
 	tick1 := 2*math.Pi/sampleRate64*freq*2
-	tick2 := 2*math.Pi/sampleRate64*freq
-	tick3 := 2*math.Pi/sampleRate64*freq*10.01
-	tick4 := 2*math.Pi/sampleRate64*freq*4
+	tick2 := 2*math.Pi/sampleRate64*freq*4
+	tick3 := 2*math.Pi/sampleRate64*freq*10
 	for i, _ := range buf {
-		bar := 70.0*math.Sin(timer)
-		bar1 := 30.0*math.Sin(timer1)
-		bar2 := bar1*math.Cos(timer2)*math.Cos(timer1)
-		bar3 := bar2*math.Cos(timer3)*math.Sin(timer1)
-		buf[i] = 127 + byte(bar-bar1-bar2+bar3)
+		bar := 60.0*math.Sin(timer)
+		bar1 := 60.0*math.Sin(timer1)
+		bar2 := (bar1/3)*math.Sin(timer2)
+		bar3 := (bar2)*math.Sin(timer3)*math.Cos(timer)
+		buf[i] = 127 + byte(bar-bar1-bar2-bar3)
 		timer += tick
 		timer1 += tick1
 		timer2 += tick2
 		timer3 += tick3
-		timer4 += tick4
 	}
 
 	/*
