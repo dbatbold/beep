a6a98eeab8d65955b89cc433607465da47170c39
Batbold Dashzeveg <vimcode@gmail.com>
2015-01-12 16:59:52 -0500

    removed old code

--- piano.go
+++ piano.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"math"
 )
 
-var pianoNoteC2buf []byte
-
 type Piano struct {
 	Freq float64
-}
-
-// Simulates waveform of piano string
-func (p *Piano) GenerateWave(force byte) []byte {
-	samples := int(sampleRate64 / p.Freq)
-	tick := 2.0 * math.Pi / float64(samples)
-	bufString := make([]byte, samples)
-	bufWaveLimit := 1024 * 1024 * 1
-	var bufWave bytes.Buffer
-
-	timer := 0.0
-	for i := 0; i < 500; i++ {
-		for s := 0; s < samples; s++ {
-			vib := 5.0 * math.Sin(timer/10)
-			amp := 10.0 * math.Sin(timer/20)
-			bar := 127 + byte(vib*100.0*math.Sin(timer)+amp)
-			bufString[s] = bar
-			timer += tick
-		}
-		bufWave.Write(bufString)
-		if bufWave.Len() > bufWaveLimit {
-			// too big
-			break
-		}
-	}
-	return bufWave.Bytes()
 }
 
 func (p *Piano) GenerateNote(freq float64, duration int) []byte {
@@ -58,19 +29,5 @@ func (p *Piano) GenerateNote(freq float64, duration int) []byte {
 		timer2 += tick2
 		timer3 += tick3
 	}
-
-	/*
-		opt := os.O_WRONLY|os.O_CREATE|os.O_TRUNC
-		sampleFile, err := os.OpenFile("test.wav", opt, 0644)
-		if err != nil {
-			panic(err)
-		}
-		bufLen := len(buf)
-		defer sampleFile.Close()
-		header := NewWaveHeader(1, 44100, 8, bufLen)
-		header.WriteHeader(sampleFile)
-		sampleFile.Write(buf)
-	*/
-
 	return trimWave(buf)
 }
--- piano_test.go
+++ piano_test.go
@@ -1,64 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os"
-	"testing"
 )
 
-// channel:
-// byte 0,1 - left audio
-// byte 2,3 - right audio
-func TestFormatPianoSample(t *testing.T) {
-	var header WaveHeader
-	var bufWave bytes.Buffer
-	opt := os.O_RDONLY
-	sampleFile, err := os.OpenFile("piano.wav", opt, 0644)
-	if err != nil {
-		t.Error(err)
-	}
-	defer sampleFile.Close()
-	n, err := header.ReadHeader(sampleFile)
-	if err != nil {
-		t.Error(err)
-	}
-	//fmt.Println(header.String())
-
-	var buf [1024 * 10]byte
-	for {
-		n, err = sampleFile.Read(buf[:])
-		if err != nil {
-			break
-		}
-		bufWave.Write(buf[:n])
-	}
-	fmt.Println(header.String())
-	//t.Log("SIZE", bufWave.Len())
-	//fmt.Println(bufWave.Len())
-	//fmt.Printf("%x\n", md5.Sum(bufWave.Bytes()))
-
-	var left16 int16
-	var right16 int16
-	width := 76
-	for i, bar := range bufWave.Bytes() {
-		switch i % 4 {
-		case 0:
-			if i%width == 0 {
-				fmt.Print("\"")
-			}
-			left16 = int16(bar)
-		case 1:
-			left16 += int16(bar) << 8
-		case 2:
-			right16 = int16(bar)
-		case 3:
-			right16 += int16(bar) << 8
-			//fmt.Printf("%v ", left16)
-			fmt.Printf("%.4x", uint16(0xffff/2+int(left16)))
-			if i%width == width-1 {
-				fmt.Println("\",")
-			}
-		}
-	}
+func TestPiano(t *testing.T) {
 }
